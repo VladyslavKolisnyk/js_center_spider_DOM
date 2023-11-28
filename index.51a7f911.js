f99b3d89e6fe6df0b468d3bf029c4531ddf3d71e
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-11-28 12:40:05 +0200

    make build

---
+++ index.51a7f911.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.top="".concat(t.clientHeight/2-e.offsetHeight/2,"px"),e.style.left="".concat(t.clientWidth/2-e.offsetWidth/2,"px");
+//# sourceMappingURL=index.51a7f911.js.map
